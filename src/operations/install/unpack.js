d056fc7f56125de10d91937ceb8f0169ced6eda8
Kumar-Saksham <38045032+Kumar-Saksham@users.noreply.github.com>
2020-04-17 18:35:27 +0530

    Add tests for install operations

--- src/operations/install/unpack.js
+++ src/operations/install/unpack.js
@@ -5,12 +5,22 @@ const Err = require("../../helperFunctions/err");
 const Zip = require("adm-zip");
 
 const unzip = async (source, destination) => {
-  if (!(await fs.stat(source)).isFile()) {
-    throw new Error("Source is not a file");
+  if (!path.isAbsolute(source)) {
+    throw new Error("Source is not absolute");
   }
+  if (!path.isAbsolute(destination)) {
+    throw new Error("Destination is not absolute");
+  }
+  try {
+    if (!(await fs.stat(source)).isFile()) {
+      throw new Error("Not a file");
+    }
+  } catch (e) {
+    throw new Error(`Source: ${e.message}`);
+  }
+
   await fs.ensureDir(destination);
   const zip = new Zip(source);
-
   let outputDirectory;
 
   zip.extractAllTo(destination, true);
